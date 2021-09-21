ea52602d356a93a6765656aa76197c289662cd0a
claire_bouillot <claire.bouillot12@gmail.com>
2021-09-21 09:31:13 +0200

    Fini TP3

--- app/src/main/java/com/example/tp3/InfoPokemon.java
+++ app/src/main/java/com/example/tp3/InfoPokemon.java
@@ -1,4 +1,26 @@
 package com.example.tp3;
 
-public class InfoPokemon {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
+public class InfoPokemon extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_pokemon);
+        String editTextPokemonName = getIntent().getStringExtra("inputpokemonname");
+        TextView InfoPoke = findViewById(R.id.info);
+        InfoPoke.setText(editTextPokemonName);
+    }
+
+    public void buttonGoBack(View view) {
+        onBackPressed();
+    }
 }
--- app/src/main/java/com/example/tp3/MainActivity.java
+++ app/src/main/java/com/example/tp3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
 import android.widget.EditText;
 import android.content.Intent;
 import android.view.View;
@@ -28,24 +29,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OnClick_search(android.view.View v){
+        //Log.d("MainActivity", editTextPokemonName.getText().toString());
         Intent intent;
         intent = new Intent(this, InfoPokemon.class);
-        intent.putExtra("inputpokemonname", (Parcelable) editTextPokemonName);
+        intent.putExtra("inputpokemonname", editTextPokemonName.getText().toString());
         startActivity(intent);
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        String value = editTextPokemonName.toString();
-        outState.putString("editTextPokemonName", value);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        String PokeName;
-        if(savedInstanceState.containsKey("editTextPokemonName"))
-            PokeName = savedInstanceState.getString("editTextPokemonName");
-        else
-            PokeName = "";
     }
 }
